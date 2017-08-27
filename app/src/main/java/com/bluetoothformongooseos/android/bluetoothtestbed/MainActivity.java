1d04e83b5dc3beecf59527f7bd0db1af846af962
applecrusher <kappel1992@gmail.com>
2017-08-27 17:43:22 -0400

    changed the build and fixed error

--- app/src/main/java/com/bluetoothformongooseos/android/bluetoothtestbed/MainActivity.java
+++ app/src/main/java/com/bluetoothformongooseos/android/bluetoothtestbed/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.bluetoothformongooseos.android.bluetoothtestbed.R;
 import com.bluetoothformongooseos.android.bluetoothtestbed.client.ClientActivity;
@@ -18,7 +19,16 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
 
-        binding.launchClientButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this,
-                ClientActivity.class)));
+       /* binding.launchClientButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this,
+                ClientActivity.class)));*/
+
+
+
+        binding.launchClientButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this,
+                        ClientActivity.class));
+            }
+        });
     }
 }
